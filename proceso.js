700beedab28d7519c88de01faf9edf03884a0823
BREINER07 <bdarias372@misena.edu.co>
2023-06-29 08:49:37 -0500

    first commit

---
+++ proceso.js
@@ -0,0 +1,25 @@
+function tico() {
+    document.getElementById('pie').innerHTML= ""
+    +"<div><ul><li><h1>Estadio Arena Castelão</h1></li></ul></div><br>"
+    +"<div><ul><li>El recinto fue inaugurado el 11 de noviembre de 1973 y posee una capacidad máxima para 65 000 espectadores sentados tras su última remodelación en 2013</li></ul></div> ";
+}
+function tica() {
+    document.getElementById('pio').innerHTML= ""
+    +"<div><ul><li><h1>Estadio Beira-Rio</h1></li></ul></div><br>"
+    +"<div><ul><li>conocido también como José Pinheiro Borda es un estadio de fútbol brasileño ubicado en la ciudad de Porto Alegre, en la costa del río Guaiba, estado de Rio Grande do Sul. El equipo al que pertenece es el Internacional</li></ul></div>";
+ }
+function ticy() {
+    document.getElementById('piu').innerHTML= ""
+    +"<div><ul><li><h1>Estadio Mineirão</h1></li></ul></div><br>"
+    +"<div><ul><li>Inaugurado en 1965, el Mineirão tiene capacidad actual para 60 000 espectadores. Sufrió una importante reducción en su capacidad para poder acogerse a las normas de la FIFA para los estadios de fútbol.</li></ul></div>";
+ }
+ function ticu() {
+    document.getElementById('pia').innerHTML= ""
+    +"<div><ul><li><h1>Estadio de Maracaná</h1></li></ul></div><br>"
+    +"<div><ul><li>Es el estadio más grande de Brasil y fue el más grande del mundo durante mucho tiempo. Inició su construcción a principios de 1949 como parte de los esfuerzos locales por obtener la sede de la Copa Mundial de Fútbol</li></ul></div>";
+ }
+ function ticb() {
+    document.getElementById('pik').innerHTML= ""
+    +"<div><ul><li><h1>Estadio de brasilia</h1></li></ul></div><br>"
+    +"<div><ul><li>es un estadio multiusos ubicado en Brasília en el Distrito Federal en Brasil. Fue inaugurado en 1974 y tras la última remodelación, de 2013, cuenta con una capacidad de 72 800 espectadores sentados.</li></ul></div>";
+ }
